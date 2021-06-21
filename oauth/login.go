9074f605d2f04d8c3f29f767a44d881b4a52e6b2
Robert Barrow <merefield@gmail.com>
2021-06-21 13:05:26 +0000

    FEATURE: upon login, default role to recorded user role

--- oauth/login.go
+++ oauth/login.go
@@ -1,6 +1,12 @@
 package oauth
 
-import "github.com/resonatecoop/user-api/model"
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/resonatecoop/user-api/model"
+)
 
 // Login creates an access token and refresh token for a user (logs him/her in)
 func (s *Service) Login(client *model.Client, user *model.User, scope string) (*model.AccessToken, *model.RefreshToken, error) {
@@ -8,6 +14,12 @@ func (s *Service) Login(client *model.Client, user *model.User, scope string) (*
 	if user != nil && !s.IsRoleAllowed(user.RoleID) {
 		// For security reasons, return a general error message
 		return nil, nil, ErrInvalidUsernameOrPassword
+	}
+
+	scope, err := s.updateUserScopeWithRole(user, scope)
+
+	if err != nil {
+		return nil, nil, err
 	}
 
 	// Create a new access token
@@ -33,4 +45,30 @@ func (s *Service) Login(client *model.Client, user *model.User, scope string) (*
 	}
 
 	return accessToken, refreshToken, nil
+}
+
+func (s *Service) updateUserScopeWithRole(user *model.User, scope string) (string, error) {
+
+	ctx := context.Background()
+
+	scopes := strings.Split(scope, " ")
+
+	if scopes[0] != "read" && scopes[0] != "read_write" {
+		return "", errors.New("invalid scope format")
+	}
+
+	scopeRole := new(model.Role)
+
+	err := s.db.NewSelect().
+		Model(scopeRole).
+		Where("id = ?", user.RoleID).
+		Scan(ctx)
+
+	if err != nil {
+		return "", errors.New("problem determining role from user record")
+	}
+
+	scope = scopes[0] + " " + scopeRole.Name
+
+	return scope, nil
 }
